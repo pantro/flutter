10f7763df24d3904d6f584825e6a43472a48e97e
Gian Condori Luna <francoguns16@gmail.com>
2024-08-23 19:58:48 -0300

    23-282: Pantalla de tarjeta seleccionada

--- stripe_app/lib/pages/home_page.dart
+++ stripe_app/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:stripe_app/data/tarjetas.dart';
+import 'package:stripe_app/widgets/total_pago_button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -45,6 +46,10 @@ class HomePage extends StatelessWidget {
                 );
               }
             )
+          ),
+          const Positioned(
+            bottom: 0,
+            child: TotalPagoButton(),
           )
         ],
       )
