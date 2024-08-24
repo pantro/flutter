2893cd4fc3c2d391019779e41593012a6f2a7558
Gian Condori Luna <francoguns16@gmail.com>
2024-08-23 21:15:11 -0300

    23-283: Pantalla de tarjeta seleccionada

--- stripe_app/lib/pages/home_page.dart
+++ stripe_app/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:stripe_app/data/tarjetas.dart';
+import 'package:stripe_app/helpers/helpers.dart';
+import 'package:stripe_app/pages/tarjeta_page.dart';
 import 'package:stripe_app/widgets/total_pago_button.dart';
 
 class HomePage extends StatelessWidget {
@@ -36,13 +38,23 @@ class HomePage extends StatelessWidget {
 
                 final tarjeta = tarjetas[i];
 
-                return CreditCardWidget(
-                  cardNumber: tarjeta.cardNumberHidden, 
-                  expiryDate: tarjeta.expiracyDate, 
-                  cardHolderName: tarjeta.cardHolderName, 
-                  cvvCode: tarjeta.cvv, 
-                  showBackView: false, 
-                  onCreditCardWidgetChange: (CreditCardBrand brand) {},
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(context, navegarFadeIn(context, TarjetaPage()));
+                  },
+                  child: Hero(
+                    tag: tarjeta.cardNumber,
+                    child: CreditCardWidget(
+                      isSwipeGestureEnabled: false,
+                      isHolderNameVisible: true,
+                      cardNumber: tarjeta.cardNumberHidden, 
+                      expiryDate: tarjeta.expiracyDate, 
+                      cardHolderName: tarjeta.cardHolderName, 
+                      cvvCode: tarjeta.cvv, 
+                      showBackView: false, 
+                      onCreditCardWidgetChange: (CreditCardBrand brand) {},
+                    ),
+                  ),
                 );
               }
             )
--- stripe_app/lib/pages/tarjeta_page.dart
+++ stripe_app/lib/pages/tarjeta_page.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'package:stripe_app/models/tarjeta_credito.dart';
+import 'package:stripe_app/widgets/total_pago_button.dart';
 
 class TarjetaPage extends StatelessWidget {
-  const TarjetaPage({super.key});
+  TarjetaPage({super.key});
+
+  final tarjeta = TarjetaCredito(
+    cardNumberHidden: '4242', 
+    cardNumber: '42424242', 
+    brand: 'visa', 
+    cvv: '213', 
+    expiracyDate: '01/25', 
+    cardHolderName: 'Fernando'
+  );
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: Center(
-        child: Text('TarjetaPage'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Regresar a Pagar'),
       ),
+      body: Stack(
+        children: [
+          Container(),
+          Hero(
+            tag: tarjeta.cardNumber,
+            child: CreditCardWidget(
+              isSwipeGestureEnabled: false,
+              isHolderNameVisible: true,
+              cardNumber: tarjeta.cardNumberHidden, 
+              expiryDate: tarjeta.expiracyDate, 
+              cardHolderName: tarjeta.cardHolderName, 
+              cvvCode: tarjeta.cvv, 
+              showBackView: false, 
+              onCreditCardWidgetChange: (CreditCardBrand brand) {},
+            ),
+          ),
+          const Positioned(
+            bottom: 0,
+            child: TotalPagoButton()
+          ),
+        ],
+      )
     );
   }
 }
