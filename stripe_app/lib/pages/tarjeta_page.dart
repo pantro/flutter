82ff5b10f949b67a1d6172183f6fb806d5364fe7
Gian Condori Luna <francoguns16@gmail.com>
2024-08-28 17:39:54 -0300

    23-292: Se quedo en esa aua por que la libreria striper_payment fue deprecada y no se podia seguir con el tutorial

--- stripe_app/lib/pages/tarjeta_page.dart
+++ stripe_app/lib/pages/tarjeta_page.dart
@@ -3,23 +3,17 @@ import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:stripe_app/bloc/pagar/pagar_bloc.dart';
-import 'package:stripe_app/models/tarjeta_credito.dart';
 import 'package:stripe_app/widgets/total_pago_button.dart';
 
 class TarjetaPage extends StatelessWidget {
-  TarjetaPage({super.key});
-
-  final tarjeta = TarjetaCredito(
-    cardNumberHidden: '4242', 
-    cardNumber: '42424242', 
-    brand: 'visa', 
-    cvv: '213', 
-    expiracyDate: '01/25', 
-    cardHolderName: 'Fernando'
-  );
+  
+  const TarjetaPage({super.key});
 
   @override
   Widget build(BuildContext context){
+
+    final tarjeta = context.read<PagarBloc>().state.tarjeta;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Regresar a Pagar'),
@@ -35,7 +29,7 @@ class TarjetaPage extends StatelessWidget {
         children: [
           Container(),
           Hero(
-            tag: tarjeta.cardNumber,
+            tag: tarjeta!.cardNumber,
             child: CreditCardWidget(
               isSwipeGestureEnabled: false,
               isHolderNameVisible: true,
